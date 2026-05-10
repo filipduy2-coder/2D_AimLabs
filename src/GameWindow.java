95ae77dfcc26d2074d6159aee860e6fb30274665
filip <filip.duy2@gmail.com>
2026-05-10 22:46:54 +0200

    fix mouse movement added background adjust font size to match screen size TODO: fix background to match any screen size

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -10,15 +11,19 @@ public class GameWindow extends JPanel implements ActionListener, MouseListener 
     int centerX;
     int centerY;
     Point centerPoint;
-    Robot robot = new Robot();
     int mouseDeltaX = 0;
     int mouseDeltaY = 0;
-    double worldOffsetX = 0;
-    double worldOffsetY = 0;
+
+    int cameraX = 0;
+    int cameraY = 0;
+
     double sensivity = 0.3;
+
+    Robot robot;
 
     ArrayList <Block> targetList;
     Image targetImg;
+    Image backgroundImg;
     int targetWidth = 80;
     int targetHeight = 80;
     int targetX;
@@ -39,16 +44,25 @@ public class GameWindow extends JPanel implements ActionListener, MouseListener 
 
         targetList = new ArrayList<>();
         targetImg = new ImageIcon("target.png").getImage();
+        backgroundImg = new ImageIcon("background.png").getImage();
 
         gameLoop = new Timer(1000/240, this);
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        BufferedImage img = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        Cursor blank = toolkit.createCustomCursor(img, new Point(0, 0), "blank");
+        setCursor(blank);
+
         addMouseListener(this);
         addMouseMotionListener(new MouseMotionAdapter() {
             public void mouseMoved(MouseEvent e) {
-                GameWindow.this.mouseMoved(e);
+                centerX = getWidth() / 2;
+                centerY = getHeight() / 2;
+                calculateDeltaMousemovement(e.getX(), e.getY());
+                Point p = getLocationOnScreen();
+                robot.mouseMove(p.x + centerX, p.y +  centerY);
             }
         });
-        //  setCursor(getToolkit().createCustomCursor(
-        //      new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB), new Point(0, 0), "blank"));
     }
 
     void placeTarget() {
@@ -70,14 +84,27 @@ public class GameWindow extends JPanel implements ActionListener, MouseListener 
     }
 
     void draw(Graphics g) {
+        g.drawImage(backgroundImg,0, 0, getWidth(), getHeight(),
+                cameraX, cameraY, cameraX + getWidth(), cameraY + getHeight(),
+                null);
 
         for (Block target : targetList) {
             g.drawImage(target.getImage(), target.getX(), target.getY(), null);
         }
-        g.setColor(Color.black);
-        g.setFont(new Font("Courier", Font.PLAIN, 40));
-        g.drawString("Score: " + score, centerX, 50);
-        g.drawString("Time Remaining: " + timeRemaining, 400, 50);
+
+        int fontSize = Math.max(6, getWidth() / 50);
+        g.setFont(new Font("Arial", Font.BOLD, fontSize));
+        FontMetrics fm = g.getFontMetrics();
+        String scoreText = "Score: " + score;
+        int scoreX = (getWidth() - fm.stringWidth(scoreText)) / 2;
+        int scoreY = fm.getHeight();
+        g.drawString(scoreText, scoreX, scoreY);
+
+        String timeText = "Time Remaining: " + timeRemaining;
+        int timeX = getWidth() - fm.stringWidth(timeText) - 10 ;
+        int timeY = fm.getHeight();
+        g.drawString(timeText, timeX, timeY);
+
         // crosshair
         int thickness = 3;
         int size = 10;
@@ -90,11 +117,6 @@ public class GameWindow extends JPanel implements ActionListener, MouseListener 
         g.fillRect(centerX + gap/2, centerY - thickness/2, size, thickness);
     }
 
-    void mouseMoved(MouseEvent e) {
-        mouseDeltaX = e.getX() - centerX;
-        mouseDeltaY = e.getY() - centerY;
-        robot.mouseMove(centerX, centerY);
-    }
 
     boolean collision(Block a) {
         return a.getBounds().contains(centerPoint);
@@ -103,8 +125,6 @@ public class GameWindow extends JPanel implements ActionListener, MouseListener 
     long start = System.currentTimeMillis();
     void update() {
         if (!gameOver) {
-            centerX = getWidth() / 2;
-            centerY = getHeight() / 2;
             centerPoint = new Point(centerX, centerY);
             long elapsed = System.currentTimeMillis() - start;
             timeRemaining = Math.toIntExact((gameTimeLimit - elapsed));
@@ -112,8 +132,11 @@ public class GameWindow extends JPanel implements ActionListener, MouseListener 
                 gameOver = true;
                 return;
             }
-            worldOffsetX -= mouseDeltaX * sensivity;
-            worldOffsetY -= mouseDeltaY * sensivity;
+            cameraX += (int) (mouseDeltaX * sensivity);
+            cameraY += (int) (mouseDeltaY * sensivity);
+            cameraX = Math.max(0, Math.min(cameraX, backgroundImg.getWidth(null) - getWidth()));
+            cameraY = Math.max(0, Math.min(cameraY, backgroundImg.getHeight(null) - getHeight()));
+
             if (!targetList.isEmpty() && collision(targetList.getFirst()) && isClicked) {
                 score++;
             }
@@ -121,6 +144,11 @@ public class GameWindow extends JPanel implements ActionListener, MouseListener 
                 isClicked = false;
             }
         }
+    }
+
+    public void calculateDeltaMousemovement(int mouseX, int mouseY) {
+        mouseDeltaX = mouseX - centerX;
+        mouseDeltaY = mouseY - centerY;
     }
 
     public void addNotify() {
